5658e805755e65abcc8826e9916e60f709db3bb2
Nicolas Dos Santos <135163531+Nidossantos@users.noreply.github.com>
2023-11-15 23:27:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Datos de habilidades
-    var habilidadesData = {
-        labels: ["C", "Python", "HTML", "CSS", "Excel", "Word", "PowerPoint", "Ingles", "Portugues"],
+    var datosLenguajes = {
+        labels: ['C', 'Python', 'HTML', 'CSS'],
         datasets: [{
-            label: 'Nivel de habilidades',
-            data: [4, 3, 2, 2, 5, 5, 3, 4, 3], // Niveles
-            backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 128, 0, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(99, 255, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 128, 0, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 0, 0, 0.2)'],
-            borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(255, 128, 0, 1)', 'rgba(54, 162, 235, 1)', 'rgba(99, 255, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 128, 0, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 0, 0, 1)'],
-            borderWidth: 1
+            data: [4, 3, 2, 2], // Puedes ajustar estos valores según sea necesario
+            backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 128, 0, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+            borderColor:['rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(255, 128, 0, 1)', 'rgba(54, 162, 235, 1)'],
+            borderWidth:1
         }]
     };
 
-    // Configuración del gráfico de habilidades
-    var habilidadesConfig = {
-        type: 'bar', // Cambiado a 'bar' para gráfico de barras verticales
-        data: habilidadesData,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 6
-                },
-            }
-        }
+    var datosMSOffice = {
+        labels: ['Excel', 'Word', 'PowerPoint'],
+        datasets: [{
+            data: [4, 5, 3], // Puedes ajustar estos valores según sea necesario
+            backgroundColor: ['rgba(99, 255, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 128, 0, 0.2)'],
+            borderColor:['rgba(99, 255, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 128, 0, 1)'],
+            borderWidth:1
+        }]
     };
 
-    // Crear el gráfico de habilidades
-    var habilidadesChart = new Chart(document.getElementById('habilidades'), habilidadesConfig);
+    var datosIdiomas = {
+        labels: ['Inglés', 'Portugués'],
+        datasets: [{
+            data: [4, 3], // Puedes ajustar estos valores según sea necesario
+            backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(255, 0, 0, 0.2)'],
+            borderColor:['rgba(54, 162, 235, 1)', 'rgba(255, 0, 0, 1)'],
+            borderWidth:1
+        }]
+    };
+
+    // Configuración común para todos los gráficos
+    var config = {
+        type: 'bar',
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+        },
+    };
+
+    // Dibujar gráficos
+    var ctxLenguajes = document.getElementById('graficoLenguajes').getContext('2d');
+    new Chart(ctxLenguajes, Object.assign({}, config, { data: datosLenguajes }));
+
+    var ctxMSOffice = document.getElementById('graficoMSOffice').getContext('2d');
+    new Chart(ctxMSOffice, Object.assign({}, config, { data: datosMSOffice }));
+
+    var ctxIdiomas = document.getElementById('graficoIdiomas').getContext('2d');
+    new Chart(ctxIdiomas, Object.assign({}, config, { data: datosIdiomas }));
 });
